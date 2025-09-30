2c99cb73d3e84b0287f3f121262d4aa425083804
pyjoek <joelnrlson@gmail.com>
2025-09-30 21:15:46 +0300

    ui going great

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     if (!newCamera.name || !newCamera.ip) return alert("Fill name & IP");
 
     try {
-      const res = await fetch(`http://localhost:8000/nvrs/${selectedNvr}/cameras`, {
+      const res = await fetch(`http://localhost:5000/nvrs/${selectedNvr}/cameras`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newCamera),
