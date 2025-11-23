22ee1ad6aaa1b45a2c9a2fc799dca28c78851ca7
pyjoek <joelnrlson@gmail.com>
2025-11-23 17:36:05 +0300

    now having the structure of the app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -22,14 +22,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  // The pages you want to switch between
+  final List<Widget> _pages = <Widget>[
+    Center(child: Text("Home Page")),
+    Center(child: Text("Camera Page")),
+    Center(child: Text("Settings Page")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Camera Monitor'),
       ),
-      body: Center(
-        child: Text('Welcome to Camera Monitor!'),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: 'Camera',
+          ),        BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
